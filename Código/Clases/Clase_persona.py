647829f656a2eca58555732e6e04d728ee114a6e
Art <brandonalvarez1011@gmail.com>
2026-06-16 16:13:39 -0600

    Eliminación de validaciones en el constructor de la clase Persona

--- Código/Clases/Clase_persona.py
+++ Código/Clases/Clase_persona.py
@@ -1,21 +1,9 @@
 class Persona:
     def __init__(self, nombre, apellido, fecha_nacimiento, nacionalidad):
-
-        if not isinstance(nombre, str):
-            return "El nombre debe de ser un string."
-
-        if not isinstance(apellido, str):
-            return "El apellido debe de ser un string."
-
-        if not isinstance(fecha_nacimiento, str):
-            return (
-                "La fecha de nacimiento debe de ser un string de la forma DD/MM/AAAA."
-            )
-
-        if not isinstance(nacionalidad, str):
-            return "La nacionalidad debe de ser un string."
-
         self.nombre = nombre
         self.apellido = apellido
         self.fecha_nacimiento = fecha_nacimiento
         self.nacionalidad = nacionalidad
+
+    def mostrar_datos(self):
+        return self.nombre, self.apellido, self.fecha_nacimiento, self.nacionalidad
