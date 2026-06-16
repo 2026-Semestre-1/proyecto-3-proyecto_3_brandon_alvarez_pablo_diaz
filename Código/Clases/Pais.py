7c7bf76188c3aa208ed71a6db1b2b9ba9139820b
Art <brandonalvarez1011@gmail.com>
2026-06-16 16:31:48 -0600

    Se añadio "actualizar_datos" a la clase Pais.

--- Código/Clases/Pais.py
+++ Código/Clases/Pais.py
@@ -17,3 +17,24 @@ class Pais:
         self.nombre = nombre
         self.continente = continente
         self.ranking = ranking_fifa
+
+    def actualizar_datos(self, n_codigo_fifa, n_nombre, n_continente, n_ranking_fifa):
+
+        if not isinstance(n_codigo_fifa, str):
+            return "El codigo de fifa debe de ser un string."
+
+        if not isinstance(n_nombre, str):
+            return "El nombre debe de ser un string."
+
+        if not isinstance(n_continente, str):
+            return "El continente debe de ser un string."
+
+        if not isinstance(n_ranking_fifa, int):
+            return "El ranking de la fifa debe de ser un número entero."
+
+        self.codigo = n_codigo_fifa
+        self.nombre = n_nombre
+        self.continente = n_continente
+        self.ranking = n_ranking_fifa
+
+        return True
