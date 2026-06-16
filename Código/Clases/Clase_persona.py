dbd0ae7210f267ea139eb522adcdc3e7df0aff19
Art <brandonalvarez1011@gmail.com>
2026-06-16 16:22:28 -0600

    Mejora la validación de parámetros en el constructor de la clase Persona y actualiza el método mostrar_datos para devolver un diccionario.

--- Código/Clases/Clase_persona.py
+++ Código/Clases/Clase_persona.py
@@ -1,9 +1,29 @@
 class Persona:
     def __init__(self, nombre, apellido, fecha_nacimiento, nacionalidad):
+
+        if not isinstance(nombre, str):
+            raise TypeError("El nombre debe de ser un string.")
+
+        if not isinstance(apellido, str):
+            raise TypeError("El apellido debe de ser un string.")
+
+        if not isinstance(fecha_nacimiento, str):
+            raise TypeError(
+                "La fecha de nacimiento debe de ser un string de la forma DD/MM/AAAA."
+            )
+
+        if not isinstance(nacionalidad, str):
+            raise TypeError("La nacionalidad debe de ser un string.")
+
         self.nombre = nombre
         self.apellido = apellido
         self.fecha_nacimiento = fecha_nacimiento
         self.nacionalidad = nacionalidad
 
     def mostrar_datos(self):
-        return self.nombre, self.apellido, self.fecha_nacimiento, self.nacionalidad
+        return {
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "fecha_nacimiento": self.fecha_nacimiento,
+            "nacionalidad": self.nacionalidad,
+        }
