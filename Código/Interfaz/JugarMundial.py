27c4e5778d6bc3a14ddf81e2164a96fcaefea37d
Pablo Díaz Masis <p.diaz.1@estudiantec.cr>
2026-06-28 00:25:33 -0600

    Añade funcionalidad para simular la fase de grupos en VentanaJugarMundial; incluye manejo de resultados y actualización de botones.

--- Código/Interfaz/JugarMundial.py
+++ Código/Interfaz/JugarMundial.py
@@ -18,7 +18,7 @@ class VentanaJugarMundial(ctk.CtkFrame):
         self.controles.pack(pady=10, fill="x", padx=20)
 
         #boton para fase de grupos
-        self.btn_grupos = ctk.CTkButton(self.controles, text="Simular fase de grupos") #falta poner cosas
+        self.btn_grupos = ctk.CTkButton(self.controles, text="Simular fase de grupos", command=self.simular_fase_grupos) #falta poner color
         self.btn_grupos.pack(side="left", padx=15, pady=10)
 
         #boton para la siguiente ronda
@@ -29,4 +29,42 @@ class VentanaJugarMundial(ctk.CtkFrame):
         self.scroll_resultados = ctk.CTkScrollableFrame(self, width=700, height=400)
         self.scroll_resultados.pack(padx=20, pady=15, fill="both", expand=True)
 
+    def simular_fase_grupos(self):
+        if self.mi_mundial == None or self.mi_mundial.grupos == []:
+            self.imprimir_en_pantalla("Error: no se ha configurado los grupos del Mundial aún\n")
+            return
         
+        self.imprimir_en_pantalla("Iniciando fase de grupos...")
+        exito = self.mi_mundial.jugar_fase_grupos()
+
+        if exito == True:
+
+            for grupo in self.mi_mundial.grupos:
+                texto_grupo = f"{grupo.nombre_grupo} - POSICIONES FINALES\n"
+
+                for fila in grupo.tabla:
+                    #fila[0] = nombre de la seleccion, fila[1] = puntos
+                    texto_grupo += f" - {fila[0]} : {fila[1]} Pts\n"
+
+                self.imprimir_en_pantalla(texto_grupo + "\n")
+
+
+            self.clasificados_actuales = []
+            for grupo in self.mi_mundial.grupos:
+                self.clasificados_actuales += grupo.obtener_clasificados()
+
+            self.btn_grupos.configure(state="disabled")
+            self.btn_siguiente_ronda.configure(state="normal")
+            self.imprimir_en_pantalla("Fase de grupos finalizada. Clasificados listos para las eliminatorias\n\n")
+
+
+    
+
+
+
+
+    def imprimir_en_pantalla(self, texto):
+        lbl = ctk.CTkLabel(self.scroll_resultados, text=texto, justify="left", font=("Courier New", 13), anchor="w")
+        lbl.pack(fill="x", padx=10, anchor="w")
+
+
